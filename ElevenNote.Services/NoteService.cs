22eb853b35ba8dd39926c520e0ace1dca3d61e9b
StevenGrootaert <steve.grootaert@gmail.com>
2021-01-11 12:52:43 -0500

    added Note Services and can't get the succes msg to display text.

---
+++ ElevenNote.Services/NoteService.cs
@@ -0,0 +1,59 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class NoteService   // the service layer is how ou application interacts with the database -- the service here will oush and pull notes from the database
+                               // we added references for the MOdels and the Data layers so that we can use models and properties from those projects
+                               // ?? Is this the same thing as when we added the save changes and View _bd.Products.ToList and OrderBy prod Id in the database in the ProductController for general store ??
+    {
+        private readonly Guid _userId;
+
+        public NoteService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateNote(NoteCreate model) // this will create an instance of Note. 
+        {
+            var entity =
+                new Note()
+                {
+                    OwnerId = _userId,
+                    Title = model.Title,
+                    Content = model.Content,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Notes.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<NoteListItem> GetNotes()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =     //is this SQL?
+                    ctx
+                        .Notes      // e is for entity
+                        .Where(e => e.OwnerId == _userId)
+                        .Select(e => new NoteListItem
+                        {
+                            NoteId = e.NoteId,
+                            Title = e.Title,
+                            CreatedUtc = e.CreatedUtc
+                        }
+                        );
+                return query.ToArray();
+            }
+        }
+    }
+}
--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -1,4 +1,6 @@
 ﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +13,14 @@ namespace ElevenNote.WebMVC.Controllers
     public class NoteController : Controller
     {
         // GET: Note/index
-        public ActionResult Index()
+        public ActionResult Index() // right click to add a view -- click on Index and CTRL + M+G takes you to the thing
         {
-            var model = new NoteListItem[0]; //  we are initializing a new instance of the NoteListItem as an IEnumerable with the [0] syntax.
+            //var model = new NoteListItem[0]; //  we are initializing a new instance of the NoteListItem as an IEnumerable with the [0] syntax. -- ** we modified this with the following 3 lines of code in 6.02 when we added a service **
+            //var userId = Guid.Parse(User.Identity.GetUserId());
+            //var service = new NoteService(userId);
+            var service = CreateNoteService();
+            // ^^then we refactored in step 7.01 ^^
+            var model = service.GetNotes();
             return View(model);
         }
 
@@ -27,11 +34,34 @@ namespace ElevenNote.WebMVC.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(NoteCreate model)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid) return View(model);       // I missed this exclimatin point ! and that turns it to a if modelstate is not valid -  save to DB We need a serivce? nope this code is for when the model view is NOT valid
+
+            var service = CreateNoteService();
+
+            if (service.CreateNote(model))
             {
-                // save to DB We need a serivce
-            }
-            return View(model);
+                //ViewBag.SaveResult = "Your note ws created";
+                TempData["SaveResult"] = "Your note was created."; // changed to a TempData in 7.02 TempData removes information after it's been accessed
+                return RedirectToAction("Index");
+            };
+            ModelState.AddModelError("", "Note could not be created.");
+                return View(model);
+            
+            // added in 6.02 -  method makes sure the model is valid, grabs the current userId, calls on CreateNote, and returns the user back to the index view.
+            //var userId = Guid.Parse(User.Identity.GetUserId()); -- this gets refactored (edit>refactor>extract method)
+            //var service = new NoteService(userId); -- this got refactored
+            //var service = CreateNoteService();  // changed NoteService to var Do not try to instantiate a Service inside of the constructor. The MVC framework does not have the data available that may be needed.
+
+            //service.CreateNote(model);
+
+            //return RedirectToAction("Index");
+        }
+
+        private NoteService CreateNoteService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            return service;
         }
     }
 }
