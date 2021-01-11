6d9eef3b28ddfa3826a4c79ff57312ad31104e3d
StevenGrootaert <steve.grootaert@gmail.com>
2021-01-10 19:27:27 -0500

    added Note Models for create

---
+++ ElevenNote.Models/NoteCreate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ElevenNote.Models
+{
+    public class NoteCreate // this allows us to have some validation for note properties -- These are the things that we'll need to capture when we create a note and save it to the database. nothing more and nothing less. Nothing else gets passed when creating a note
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+        [MaxLength(100, ErrorMessage = "There are too many characters in this field.")]
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -1,4 +1,4 @@
-﻿using ElevenNote.WebMVC.Models;
+﻿using ElevenNote.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +10,28 @@ namespace ElevenNote.WebMVC.Controllers
     [Authorize]
     public class NoteController : Controller
     {
-        // GET: Note
+        // GET: Note/index
         public ActionResult Index()
         {
             var model = new NoteListItem[0]; //  we are initializing a new instance of the NoteListItem as an IEnumerable with the [0] syntax.
-            return View();
+            return View(model);
+        }
+
+        // GET: Note/Create
+        public ActionResult Create()
+        {
+            return View(); // we're returning the view model that serves the create interface to input the information used to actually create the note
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+                // save to DB We need a serivce
+            }
+            return View(model);
         }
     }
 }
